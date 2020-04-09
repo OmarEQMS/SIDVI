847dfeb494cb6c54c33d4052ce53c5b1e9cc1895
Fernanda Orduna <A01422919@itesm.mx>
2020-04-09 15:07:13 -0500

    Component list-all-virus created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { VirusListAllComponent } from './components/virus-list-all/virus-list-all.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  {
+    path: 'listVirus',
+    component: VirusListAllComponent
+  },
 ];
 
 @NgModule({

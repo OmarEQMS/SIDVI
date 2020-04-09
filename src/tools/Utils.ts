f569503d9c1caa6b2ec1fc06dbf64d70af5e4880
Omar Quintero <omar.quintero.ms@gmail.com>
2020-04-09 16:21:33 -0500

    Cambios

--- src/tools/Utils.ts
+++ src/tools/Utils.ts
@@ -1,6 +1,6 @@
 import moment from 'moment-timezone';
 
-import { Defaults } from "../api";
+import { Defaults } from '../api/API';
 
 // Dates
 export function getDateNow(): Date {
@@ -21,79 +21,4 @@ export function createDate(date: Date) {
 
 export function createDateTime(date: Date) {
     return moment(date).tz(Defaults.timezone).format(Defaults.timeformat);
-}
-
-// Otros
-export function generateCode(alphabet: string, length: number): string { // 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-    let code = "";
-    for ( let i = 0; i < length; i++ ) {
-        code = code + alphabet.charAt(Math.floor(Math.random() * alphabet.length));
-    }
-    return code;
-}
-
-export function fileToBase64(mimetype: string, buffer: any): any { //string
-    return `data:${mimetype};base64,${Buffer.from(buffer).toString('base64')}`;
-}
-
-// Array Modifier
-export function shuffleArray<T>(array: Array<T>): Array<T> {
-    let currentIndex = array.length - 1;
-    let tempElement: T;
-
-    while (currentIndex >= 0){
-        let randomIndex = Math.floor(Math.random() * (currentIndex + 1)); 
-        tempElement = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = tempElement;
-        currentIndex--;
-    }
-    return array;
-}
-export function resizeArray<T>(array: Array<T>, size: number): Array<T> {
-    if(array.length>size){
-        array = array.slice(0, size);
-    }else{
-        array.push(...array.slice(0, size - array.length));
-    }
-    return array;
-}
-
-// JSON Modifier
-export function deleteProperty(data: any, deleteP: string[]){ 
-    if (typeof data === 'object') {
-        deleteP.forEach((item:string) => {
-            delete data[item]; 
-        });
-    }    
-    return data;
-}
-
-export function deleteDeepNulls(data: any) {
-    if (typeof data === 'object') {
-        Object.keys(data).forEach((key) => {
-            if (data[key] == null || (typeof data[key] === 'number' && isNaN(data[key]))) {
-                delete data[key]
-            } else {
-                deleteDeepNulls(data[key])
-            }
-        });
-    }
-    return data
-}
-
-//Substitute ref to the real schema in the specification
-export function ref2Schema(schema: any, data: any){
-    let ref;
-    if (typeof data === 'object') {        
-        Object.keys(data).forEach((key) => {
-            if (key=='$ref') {
-                ref = data[key].split('/');                  
-            }else{
-                data[key] = ref2Schema(schema, data[key]);        
-            }
-        });
-    }
-    if(ref!=undefined){ return schema[ref[ref.length-1]]; };
-    return data;
 }
--- src/tools/index.ts
+++ src/tools/index.ts
@@ -1,3 +1,2 @@
-export * from './Log';
 import * as _Utils from './Utils';
 export import Utils = _Utils;

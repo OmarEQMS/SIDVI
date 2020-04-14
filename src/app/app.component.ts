f8d8286aeb563745249829638500262834426b57
Omar Quintero <omar.quintero.ms@gmail.com>
2020-04-14 16:26:23 -0500

    administrador sin navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { ActivatedRoute, Router, ActivationStart } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,18 +11,25 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
-    this.initializeApp();
-  }
+    url: string;
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
-  }
+    constructor(
+        private platform: Platform,
+        private splashScreen: SplashScreen,
+        private statusBar: StatusBar,
+        private router: Router
+    ) {
+        this.url = '';
+        this.initializeApp();
+        router.events.subscribe((val: ActivationStart) => {
+            this.url = router.url.split('/')[1];
+        });
+    }
+
+    initializeApp() {
+        this.platform.ready().then(() => {
+        this.statusBar.styleDefault();
+        this.splashScreen.hide();
+        });
+    }
 }
--- src/app/components/estadistica/estadistica.component.ts
+++ src/app/components/estadistica/estadistica.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ubicacion } from 'src/models';
 
 @Component({
   selector: 'app-estadistica',
@@ -6,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./estadistica.component.scss'],
 })
 export class EstadisticaComponent implements OnInit {
+    ubicacion: Ubicacion;
 
-  constructor() { }
+    constructor() {
+        this.ubicacion = new Ubicacion();
+    }
 
-  ngOnInit() {}
+    ngOnInit() {}
+    ionViewWillEnter() {
+        this.ubicacion = new Ubicacion();
+    }
 
 }

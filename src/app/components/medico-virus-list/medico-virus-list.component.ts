e4054e55a461bed0c1591e74627b80c84185cea1
Omar Quintero <omar.quintero.ms@gmail.com>
2020-04-16 16:52:55 -0500

    Filñtrar Nombre

--- src/app/components/medico-virus-list/medico-virus-list.component.ts
+++ src/app/components/medico-virus-list/medico-virus-list.component.ts
@@ -8,92 +8,84 @@ import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 import { async } from '@angular/core/testing';
 
 @Component({
-  selector: 'app-medico-virus-list',
-  templateUrl: './medico-virus-list.component.html',
-  styleUrls: ['./medico-virus-list.component.scss'],
+    selector: 'app-medico-virus-list',
+    templateUrl: './medico-virus-list.component.html',
+    styleUrls: ['./medico-virus-list.component.scss'],
 })
 export class MedicoVirusListComponent implements OnInit {
-  medicosVirus: MedicoVirus[];
-  idVirus: number;
-  ubicacion: Ubicacion;
-  ubicacionesIds: number[];
+    medicosVirus: MedicoVirus[];
+    idVirus: number;
+    ubicacion: Ubicacion;
+    ubicacionesIds: number[];
+    filtrarNombre: string;
+
+    icons: { [id: string]: IconDefinition } = {
+        plus: faChevronRight,
+        minus: faChevronDown,
+        guion: faMinus
+    };
+
+    constructor(
+            private sidvi: SIDVIServices,
+            private activatedRoute: ActivatedRoute,
+            private sanitizer: DomSanitizer
+    ) {
+        this.ubicacion = new Ubicacion({ idUbicacion: -1 } as IUbicacion);
+    }
+
+    ngOnInit() { }
+    ionViewWillEnter() {
+        this.ubicacionesIds = null;
+        this.filtraUbicaciones();
+    }
 
 
-  icons: { [id: string]: IconDefinition } = {
-    plus: faChevronRight,
-    minus: faChevronDown,
-    guion: faMinus
-  };
-
-  constructor(private sidvi: SIDVIServices,
-    private activatedRoute: ActivatedRoute,
-    private sanitizer: DomSanitizer
-  ) {
-    this.ubicacion = new Ubicacion({ idUbicacion: -1 } as IUbicacion);
-  }
-
-  ngOnInit() { }
-  ionViewWillEnter() {
-    this.ubicacionesIds = new Array(0);
-    this.idVirus = parseInt(this.activatedRoute.snapshot.paramMap.get('idVirus'), 10);
-    this.sidvi.medicoVirus.listarMedicosVirus(null, this.idVirus).subscribe(
-      res => {
-        this.medicosVirus = res.resultados.map((item: any) => new MedicoVirus(item));
-        for (const medicoVirus of this.medicosVirus) {
-          medicoVirus.medico.archivoIconoImg = this.sanitizer.bypassSecurityTrustResourceUrl(medicoVirus.medico.archivoFoto as string);
-          console.log(medicoVirus.medico.archivoIconoImg);
+    filtrar() {
+        this.ubicacionesIds = new Array(0);
+        this.collectIds(this.ubicacion);
+        console.log(this.ubicacionesIds);
+        this.filtraUbicaciones();
+    }
+    async collectIds(ubicacion: Ubicacion) {
+        if (ubicacion.localSelected) {
+        this.ubicacionesIds.push(ubicacion.idUbicacion);
+        if (ubicacion.ubicaciones == null) {
+            await this.getUbicacionesHijo(ubicacion);
         }
-      },
-      err => {
-        console.log(err);
-
-      }
-    );
-  }
-
-
-  filtrar() {
-    this.ubicacionesIds = new Array(0);
-    this.collectIds(this.ubicacion);
-    console.log(this.ubicacionesIds);
-    this.filtraUbicaciones();
-  }
-  async collectIds(ubicacion: Ubicacion) {
-    if (ubicacion.localSelected) {
-      this.ubicacionesIds.push(ubicacion.idUbicacion);
-      if (ubicacion.ubicaciones == null) {
-        await this.getUbicacionesHijo(ubicacion);
-      }
-    }
-    if (ubicacion.ubicaciones == null) { return; }
-    for (const ubi of ubicacion.ubicaciones) {
-      this.collectIds(ubi);
-    }
-  }
-  async getUbicacionesHijo(ubicacion: Ubicacion) {
-    const result = await this.sidvi.ubicacion.listarUbicaciones(ubicacion.idUbicacion).toPromise();
-    ubicacion.ubicaciones = result.resultados.map((item: any) => new Ubicacion(item));
-    for (const ubi of ubicacion.ubicaciones) {
-      ubi.localSelected = ubicacion.localSelected;
-      this.sidvi.ubicacion.listarUbicaciones(ubicacion.idUbicacion).subscribe(
-        res => { if (res.total > 0) { ubi.localPadre = true; ubi.localIcono = this.icons.plus; } });
-    }
-  }
-
-  async filtraUbicaciones() {
-    this.idVirus = parseInt(this.activatedRoute.snapshot.paramMap.get('idVirus'), 10);
-    this.sidvi.medicoVirus.listarMedicosVirus(null, this.idVirus, this.ubicacionesIds).subscribe(
-      res => {
-        this.medicosVirus = res.resultados.map((item: any) => new MedicoVirus(item));
-        for (const medicoVirus of this.medicosVirus) {
-          medicoVirus.medico.archivoIconoImg = this.sanitizer.bypassSecurityTrustResourceUrl(medicoVirus.medico.archivoFoto as string);
-          console.log(medicoVirus.medico.archivoIconoImg);
         }
-      },
-      err => {
-        console.log(err);
-      }
-    );
-  }
+        if (ubicacion.ubicaciones == null) { return; }
+        for (const ubi of ubicacion.ubicaciones) {
+        this.collectIds(ubi);
+        }
+    }
+    async getUbicacionesHijo(ubicacion: Ubicacion) {
+        const result = await this.sidvi.ubicacion.listarUbicaciones(ubicacion.idUbicacion).toPromise();
+        ubicacion.ubicaciones = result.resultados.map((item: any) => new Ubicacion(item));
+        for (const ubi of ubicacion.ubicaciones) {
+        ubi.localSelected = ubicacion.localSelected;
+        this.sidvi.ubicacion.listarUbicaciones(ubicacion.idUbicacion).subscribe(
+            res => { if (res.total > 0) { ubi.localPadre = true; ubi.localIcono = this.icons.plus; } });
+        }
+    }
+
+    async filtraUbicaciones() {
+        this.idVirus = parseInt(this.activatedRoute.snapshot.paramMap.get('idVirus'), 10);
+        this.sidvi.medicoVirus.listarMedicosVirus(null, this.idVirus, this.filtrarNombre, this.ubicacionesIds).subscribe(
+        res => {
+            this.medicosVirus = res.resultados.map((item: any) => new MedicoVirus(item));
+            for (const medicoVirus of this.medicosVirus) {
+                if (medicoVirus.medico.archivoFoto != null) {
+                    medicoVirus.medico.archivoIconoImg = this.sanitizer.bypassSecurityTrustResourceUrl(
+                        medicoVirus.medico.archivoFoto as string);
+                    console.log(medicoVirus.medico.archivoIconoImg);
+                }
+            }
+        },
+        err => {
+            console.log(err);
+
+        }
+        );
+    }
 
 }

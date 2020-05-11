bec7095557cb87187a608d407b71241181632377
Omar Quintero <omar.quintero.ms@gmail.com>
2020-05-10 19:23:28 -0500

    Un paso mas cerca del exito

--- src/app/components/estadistica/estadistica.component.ts
+++ src/app/components/estadistica/estadistica.component.ts
@@ -8,16 +8,6 @@ import { DatePipe } from '@angular/common';
 import { parse } from 'querystring';
 import { GraficaEdtadistica } from 'src/app/extra/GraficaEdtadistica';
 
-class EstadisticaValue {
-    total: number;
-    seleccionados: number;
-
-    constructor(total: number, seleccionados: number) {
-        this.total = total;
-        this.seleccionados = seleccionados;
-    }
-}
-
 @Component({
   selector: 'app-estadistica',
   templateUrl: './estadistica.component.html',
@@ -27,18 +17,11 @@ export class EstadisticaComponent implements OnInit {
     ubicacion: Ubicacion;
     categorias: CategoriaEstadistica[];
 
-    categoriaSelected: CategoriaEstadistica;
-    subcategoriaSelected: SubcategoriaEstadistica;
-    categoriaSelectedGrupo: CategoriaEstadistica;
-    subcategoriaSelectedGrupo: SubcategoriaEstadistica;
-    subcategoriaEjeHorizontal: boolean;
-    agrupacionOpcion: number;
-    tipoGrafica: string;
-
     idVirus: number;
     virus: Virus;
 
     graficasEstadisticas: GraficaEdtadistica[];
+    editandoGrafica: GraficaEdtadistica;
 
     icons: { [id: string]: IconDefinition } = {
         plus: faChevronRight,
@@ -52,20 +35,19 @@ export class EstadisticaComponent implements OnInit {
     ) {
         this.ubicacion = new Ubicacion({idUbicacion: -1} as IUbicacion);
         this.virus = new Virus();
-        this.categoriaSelectedGrupo = new CategoriaEstadistica();
-        this.categoriaSelected = new CategoriaEstadistica();
-        this.subcategoriaEjeHorizontal = true;
-        this.agrupacionOpcion = 1;
+        this.graficasEstadisticas = new Array();
     }
 
     ngOnInit() {}
     async ionViewWillEnter() {
-        this.categoriaSelectedGrupo = new CategoriaEstadistica();
-        this.categoriaSelected = new CategoriaEstadistica();
-        this.graficasEstadisticas = new Array(0);
+        this.graficasEstadisticas = new Array();
+        this.graficasEstadisticas.push(new GraficaEdtadistica(this.idVirus));
+        this.editandoGrafica = this.graficasEstadisticas[0];
+
         this.idVirus = parseInt(this.activatedRoute.snapshot.paramMap.get('idVirus'), 10);
         this.ubicacion = new Ubicacion({idUbicacion: -1} as IUbicacion);
         await this.getUbicacionesHijo(this.ubicacion);
+        GraficaEdtadistica.staticUbicacion = this.ubicacion;
         this.filtrarEstadisticas();
         this.obtenerVirus();
         this.obtenerCategorias();
@@ -82,102 +64,8 @@ export class EstadisticaComponent implements OnInit {
         this.sidvi.categoriaEstadistica.listarCategoriaEstadistica().subscribe(
             categorias => {
                 this.categorias = categorias.resultados.map((item: any) => new CategoriaEstadistica(item));
-                this.categoriaSelected = this.categorias[0];
-                this.categoriaSelectedGrupo = this.categorias[0];
+                GraficaEdtadistica.staticCategorias = categorias;
         });
-    }
-
-    updateCategoria(idCategoriaSelected: string) {
-        this.categoriaSelected = this.categorias.find((item: CategoriaEstadistica) => item.idCategoriaEstadistica === parseInt(idCategoriaSelected, 10) );
-    }
-    updateSubcategoria(idSubcategoriaSelected: string) {
-        this.subcategoriaSelected = this.categoriaSelected.subcategoriaEstadisticas.find((item: SubcategoriaEstadistica) => item.idSubcategoriaEstadistica === parseInt(idSubcategoriaSelected, 10) );
-    }
-    updateCategoriaGrupo(idCategoriaSelectedGrupo: string) {
-        this.categoriaSelectedGrupo = this.categorias.find((item: CategoriaEstadistica) => item.idCategoriaEstadistica === parseInt(idCategoriaSelectedGrupo, 10) );
-    }
-    updateSubcategoriaGrupo(idSubcategoriaSelectedGrupo: string) {
-        this.subcategoriaSelectedGrupo = this.categoriaSelectedGrupo.subcategoriaEstadisticas.find((item: SubcategoriaEstadistica) => item.idSubcategoriaEstadistica === parseInt(idSubcategoriaSelectedGrupo, 10) );
-    }
-
-    filtrarEstadisticas() {
-        this.graficasEstadisticas = new Array(0);
-
-        this.sidvi.estadistica.listarEstadisticas(this.idVirus, null, null, null, null, null, null, null, 'fecha', OrderModeEnum.ASC).subscribe(
-            estadis => {
-
-        });
-    }
-
-    calculateEstadistica(ubicacion: Ubicacion): EstadisticaValue {
-        const newEstadisticaValue = new EstadisticaValue(0, 0);
-        if (ubicacion.ubicaciones != null && ubicacion.ubicaciones.length > 0) {
-            for (const ubi of ubicacion.ubicaciones) {
-                const hijoEstadisticaValue = this.calculateEstadistica(ubi);
-                newEstadisticaValue.total += hijoEstadisticaValue.total;
-                newEstadisticaValue.seleccionados += hijoEstadisticaValue.seleccionados;
-            }
-            const ubiOtros = ubicacion.ubicaciones.find((item: Ubicacion) => item.nombre === 'Otros');
-            if (ubiOtros != null) { ubiOtros.localEstadistica.valor = ubicacion.localEstadistica.valor - newEstadisticaValue.total; }
-
-            if (ubicacion.localSelected) {
-                newEstadisticaValue.seleccionados += ubiOtros ? ubiOtros.localEstadistica.valor : 0;
-            }
-        } else {
-            if (ubicacion.localSelected) {
-                newEstadisticaValue.seleccionados = ubicacion.localEstadistica.valor;
-            } else {
-                newEstadisticaValue.seleccionados = 0;
-            }
-        }
-
-        newEstadisticaValue.total = ubicacion.localEstadistica.valor;
-        ubicacion.localEstadistica.valor = newEstadisticaValue.seleccionados;
-
-        return newEstadisticaValue;
-    }
-
-
-    setEstadisticaInUbicacion(ubicacion: Ubicacion, estadistica: Estadistica) {
-        if (ubicacion.idUbicacion === estadistica.fkUbicacion) {
-            ubicacion.localEstadistica.idEstadistica = estadistica.idEstadistica;
-            ubicacion.localEstadistica.valor = estadistica.valor;
-            return;
-        }
-        if (ubicacion.ubicaciones != null && ubicacion.ubicaciones.length > 0) {
-            for (const ubi of ubicacion.ubicaciones) {
-                this.setEstadisticaInUbicacion(ubi, estadistica);
-            }
-        }
-    }
-
-    setUbicacion(ubicacion: Ubicacion, estadistica: Estadistica, main: boolean) {
-        estadistica.fkUbicacion = ubicacion.idUbicacion;
-        ubicacion.localEstadistica = new Estadistica(estadistica);
-        if (ubicacion.ubicaciones != null && ubicacion.ubicaciones.length > 0) {
-            if (!main) {
-                const otrosUbicacion = new Ubicacion({ubicacion} as IUbicacion);
-                otrosUbicacion.idUbicacion = -1;
-                otrosUbicacion.fkUbicacion = ubicacion.idUbicacion;
-                otrosUbicacion.nombre = 'Otros';
-                ubicacion.ubicaciones.push(otrosUbicacion);
-            }
-            for (const ubi of ubicacion.ubicaciones) {
-                this.setUbicacion(ubi, estadistica, false);
-            }
-        }
-    }
-
-    setUbicacionCopia(ubicacion: Ubicacion, estadistica: Estadistica) {
-        const valor = ubicacion.localEstadistica.valor;
-        estadistica.fkUbicacion = ubicacion.idUbicacion;
-        ubicacion.localEstadistica = new Estadistica(estadistica);
-        ubicacion.localEstadistica.valor = valor;
-        if (ubicacion.ubicaciones != null && ubicacion.ubicaciones.length > 0) {
-            for (const ubi of ubicacion.ubicaciones) {
-                this.setUbicacionCopia(ubi, estadistica);
-            }
-        }
     }
 
     async getUbicacionesHijo(ubicacion: Ubicacion) {
@@ -189,6 +77,23 @@ export class EstadisticaComponent implements OnInit {
             ubi.localSelected = ubicacion.localSelected;
             await this.getUbicacionesHijo(ubi);
         }
+    }
+
+    updateCategoria(idCategoriaSelected: string) {
+        this.editandoGrafica.categoriaSelected = this.categorias.find((item: CategoriaEstadistica) => item.idCategoriaEstadistica === parseInt(idCategoriaSelected, 10) );
+    }
+    updateSubcategoria(idSubcategoriaSelected: string) {
+        this.editandoGrafica.subcategoriaSelected = this.editandoGrafica.categoriaSelected.subcategoriaEstadisticas.find((item: SubcategoriaEstadistica) => item.idSubcategoriaEstadistica === parseInt(idSubcategoriaSelected, 10) );
+    }
+    updateCategoriaGrupo(idCategoriaSelectedGrupo: string) {
+        this.editandoGrafica.categoriaSelectedGrupo = this.categorias.find((item: CategoriaEstadistica) => item.idCategoriaEstadistica === parseInt(idCategoriaSelectedGrupo, 10) );
+    }
+    updateSubcategoriaGrupo(idSubcategoriaSelectedGrupo: string) {
+        this.editandoGrafica.subcategoriaSelectedGrupo = this.editandoGrafica.categoriaSelectedGrupo.subcategoriaEstadisticas.find((item: SubcategoriaEstadistica) => item.idSubcategoriaEstadistica === parseInt(idSubcategoriaSelectedGrupo, 10) );
+    }
+
+    filtrarEstadisticas() {
+        this.editandoGrafica.filtrarEstadisticas(this.sidvi);
     }
 
 }

b42b052e02c23b199de1ff2cd73a619f9db7a8d0
unknown <a01421467@itesm.mx>
2020-05-14 11:13:12 -0500

    restablecer contraseña en admin

--- src/app/administrador/perfil/perfil.component.ts
+++ src/app/administrador/perfil/perfil.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import {Usuario} from '../../../models/index';
 import { SIDVIServices } from 'src/api';
 import Swal from 'sweetalert2';
 import { _APIResponse } from 'src/api/APIResponse';
 import { Router } from '@angular/router';
+import { ModalContainerComponent } from 'angular-bootstrap-md';
+
 
 @Component({
   selector: 'app-perfil',
@@ -11,18 +13,27 @@ import { Router } from '@angular/router';
   styleUrls: ['./perfil.component.scss'],
 })
 export class PerfilComponent implements OnInit {
+  @ViewChild('basicModal', { static: true }) basicModal: ModalContainerComponent;
   usuario: Usuario;
   idUsuario: number;
   fieldValidations = {
     'nombreCompleto' : true,
     'usuario': true,
     'correo': true,
-    'celular': true
+    'celular': true,
+    'contrasena': true,
+    'contrasenaCopy':true
   };
+  contrasenaAntigua: string;
+  contrasenaNueva: string;
+  contrasenaCopy: string;
 
   constructor(private router: Router, private sidvi: SIDVIServices) {
     this.usuario = new Usuario(); // se inicializa para que no marque error antes de que asigne el usuario.
-   }
+    this.contrasenaAntigua = '';
+    this.contrasenaNueva = '';
+    this.contrasenaCopy = '';
+  }
 
   ngOnInit() {
     this.idUsuario = this.sidvi.manager.usuario.idUsuario;
@@ -82,7 +93,6 @@ validateAll() {
 
 
 guardar() {
-
   delete this.usuario.archivoFoto;
   delete this.usuario.mimetypeFoto;
 
@@ -128,52 +138,46 @@ actualizarImagenPerfil() {
 }
 
 restablecer() {
-
-}
-
-eliminarConfirmation() {
-  Swal.fire({
-    title: '¿Estas seguro que deseas eliminar tu cuenta?',
-    text: '¡Esta acción es irreversible!',
-    icon: 'warning',
-    showCancelButton: true,
-    confirmButtonColor: '#3085d6',
-    cancelButtonColor: '#d33',
-    confirmButtonText: 'Eliminar',
-    cancelButtonText: 'Cancelar',
-    backdrop: false
-  }).then((result) => {
-    if (result.value) {
-        this.eliminar();
-    }
-  });
-}
-
-eliminar() {
-  this.sidvi.usuario.eliminarUsuario(this.idUsuario)
-  .subscribe( res => {
-      if (res.type === _APIResponse.TypeEnum.SUCCESS) {
-          Swal.fire({
-              title: 'Eliminado',
-              text: 'Su cuenta ha sido eliminada',
-              icon: 'success',
-              backdrop: false,
-          });
-          localStorage.clear();
-          this.router.navigate(['./login']);
-      }
-  }, err => {console.log(err); });
+   this.sidvi.usuario.obtenerUsuario(this.idUsuario).subscribe( usu => {
+      const u = new Usuario(usu);
+      this.sidvi.usuario.autenticacion(u.usuario, this.contrasenaAntigua).subscribe( auth => {
+        if (auth && auth.type === _APIResponse.TypeEnum.SUCCESS) {
+          this.fieldValidations.contrasena = true;
+          if (this.contrasenaNueva === this.contrasenaCopy && this.contrasenaNueva !== '') {
+            this.fieldValidations.contrasenaCopy = true;
+            // restablecer
+            this.sidvi.usuario.cambiarContrasena(u.usuario, this.contrasenaAntigua, this.contrasenaNueva)
+              .subscribe( res => {
+                // cerrar modal de contrasena
+                this.resetModal();
+                // mostrar modal de suc o err
+                if (res && res.type === _APIResponse.TypeEnum.SUCCESS) {
+                  Swal.fire({
+                    title: 'Felicidades', text: 'Su contraseña ha sido actualizada',
+                    icon: 'success',  backdrop: false,
+                  });
+                } else {
+                  Swal.fire({
+                    title: 'Error', text: 'Algo salio mal, intentelo más tarde',
+                    icon: 'error',  backdrop: false,
+                  });
+                }
+              });
+          } else {  this.fieldValidations.contrasenaCopy = false; }
+        } else { this.fieldValidations.contrasena = false; }
+      });
+    });
 }
 
 resetModal() {
-/*  this.basicModal.hide();
-  this.addInfoForm.reset();
-  this.addFile = null;
-  this.addFileName = 'Choose file';
-  this.addInfoForm.value.categoria = 0;
-  this.addInfoForm.controls.categoria.setValue('Categoría', { onlySelf: true });
-*/
-}
+  this.basicModal.hide();
 
+  this.contrasenaAntigua = '';
+  this.contrasenaNueva = '';
+  this.contrasenaCopy = '';
+
+  this.fieldValidations.contrasena = true;
+  this.fieldValidations.contrasenaCopy = true;
+}
 
 }
